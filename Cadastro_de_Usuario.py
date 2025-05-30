7424f01f534932ec106747acf69a7b4179483e1e
Gilson Cassende <gilsoncassende7@gmail.com>
2025-05-30 22:40:23 +0100

    Update Cadastro_de_Usuario.py

--- Cadastro_de_Usuario.py
+++ Cadastro_de_Usuario.py
@@ -1,18 +1,21 @@
+from time import sleep
 def cadastrar():
-    usuario = {}
-    usuario["nome"] = input("Nome : ")
-    usuario["email"] = input("E-mail : ")
-    while True:
-        idade = input("Idade : ").strip()
-        if idade.isdigit():
-            usuario["idade"] = int(idade)
-            break
-        else:
-            print("Digite uma idade valida!")
+   
+            
+        usuario = {}
+        usuario["nome"] = input("Nome : ")
+        usuario["email"] = input("E-mail : ")
+        while True:
+            idade = input("Idade : ").strip()
+            if idade.isdigit():
+                usuario["idade"] = int(idade)
+                break
+            else:
+                print("Digite uma idade valida!")
+            
         
+        return usuario
     
-    return usuario
-
 def mostrar_usuarios(lista):
     print("\t --USUARIOS CADASTRADOS-- ")
     for i, elem in enumerate(lista, start=1):
@@ -25,18 +28,33 @@ def mostrar_usuarios(lista):
 
 def main():
     usuarios = []
+   
+
+    nome = input("Qual é o seu nome ? : ")
+    print(f"Olá {nome} Seje benvindo ao nosso sistema de Cadastro de Usuarios")
+    print("Iniciando o Sistema.......")
+    sleep(5)
     print("----CADASTRO DE USUARIOS-----")   
     while True:
-        us = cadastrar()
-        usuarios.append(us) 
-        d = input("Cadastrar novo Usuario ? [S/N] : ").strip().upper()
-        if d != "S":
-            break
+        
+
+            us = cadastrar()
+            usuarios.append(us) 
+            d = input("Cadastrar novo Usuario ? [S/N] : ").strip().upper()
+            if d == "S":
+                continue
+            elif d == "N":
+                break
+          
+
+
     users = input("Desejas ver os Usuarios Cadastrados ? [S/N] : ").strip().upper() 
     if users == "S":
         mostrar_usuarios(usuarios)
-    else:
+    elif users == "N":
         print("Programa Encerrado!")
+                 
+            
 
 
 
@@ -47,7 +65,7 @@ if __name__== "__main__":
 
 
         
-    
+
 
 
 
